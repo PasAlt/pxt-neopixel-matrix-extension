d355b4683c78a3ca5e01c831faa1ab0ec0d59cb1
Pascal Zaugg <pascalzaugg1985@gmail.com>
2026-06-27 17:14:43 +0200

    Update neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -51,12 +51,13 @@ namespace neopixel {
         return strip;
     }
 
+    //% blockId="neopixel_matrix_direction_selector"
     //% block="%direction"
     //% group="Initialization"$
     // This is just here because I was not able
     // to create a dropdown menu for the enum
     // inside the Strip
-    export function setMatrixDirection(direction : NeoPixelMatrixDirection) {
+    export function _setMatrixDirection(direction : NeoPixelMatrixDirection) : NeoPixelMatrixDirection {
         return direction
     }
 
